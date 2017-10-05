ad75f9b550b6bc0e6723ec817d192797048d7f14
Ricardo Teixeira <ricardo_teixeira@hotmail.com.br>
2017-10-05 10:08:15 -0300

    refactor(babel): added babel and changed folder structure

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const app = express()
 
-app.use(express.static('public'))
+app.use('/', express.static(__dirname + 'index.html'))
 app.use('/src', express.static('src'))
 
 app.listen(8080, function () {
